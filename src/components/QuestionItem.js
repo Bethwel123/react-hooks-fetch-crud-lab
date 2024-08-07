7e1f4854fdd3a9e42531bf329cf4fd053e0372ed
Bethwel123 <bethwellangat654@gmail.com>
2024-08-07 15:47:01 +0300

    Added crud functionalities

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDelete, onChange }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,32 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleChange(e) {
+    // console.log(question)
+    onChange(id, parseInt(e.target.value));
+  }
+
+  function handleDeleteClick() {
+    // console.log(question)
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      // .then(()=>console.log("gone!"))
+      .then(() => onDelete(question));
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleChange}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
